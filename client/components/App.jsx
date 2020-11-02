2b09d9b1ddb99ec9f6026d0e3824424f6267cd41
Joshua Vial <joshua@enspiral.com>
2020-11-03 10:07:32 +1300

    refactoring into separate greeter

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,23 +2,12 @@ import React from 'react'
 import Greeter from './Greeter'
 
 class App extends React.Component {
-  state = {
-    greeting: "hello world",
-    showGreeting: true
-  }
-
-  toggleGreeting = () => {
-    this.setState({showGreeting: !this.state.showGreeting})
-  }
 
   render() {
-    let label = this.state.showGreeting ? 'hide' : 'show'
     return (
       <>
         <h1>React development has begun!</h1>
         <Greeter />
-        <button onClick={this.toggleGreeting}>{label}</button>
-        {this.state.showGreeting && this.state.greeting}
       </>
     )
   }
--- client/components/Greeter.jsx
+++ client/components/Greeter.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 export default class Greeter extends React.Component {
+    state = {
+        greeting: "hello world",
+        showGreeting: true
+    }
+
+    toggleGreeting = () => {
+        this.setState({showGreeting: !this.state.showGreeting})
+    }
 
     render() {
-        return (<div>Greetings</div>)
+        let label = this.state.showGreeting ? 'hide' : 'show'
+        return <div>
+            Greetings
+            <button onClick={this.toggleGreeting}>{label}</button>
+            {this.state.showGreeting && this.state.greeting}
+        </div>
     }
 }
