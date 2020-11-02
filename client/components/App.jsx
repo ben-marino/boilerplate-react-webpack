65cf892811924a6d7f320937ad753b4cc7efb383
Joshua Vial <joshua@enspiral.com>
2020-11-03 09:56:49 +1300

    progress so far

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  state = {
+    greeting: "hello world",
+    showGreeting: true
+  }
+
+  toggleGreeting = () => {
+    this.setState({showGreeting: !this.state.showGreeting})
+  }
+
+  render() {
+    return (
+      <>
+        <h1>React development has begun!</h1>
+        <button onClick={this.toggleGreeting}>Click</button>
+        {this.state.showGreeting && this.state.greeting}
+      </>
+    )
+  }
 }
 
 export default App
