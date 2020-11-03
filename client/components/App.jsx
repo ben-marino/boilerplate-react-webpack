44ffa6f0f4847e7bb377684aad81bce1e6002ab4
Joshua Vial <joshua@enspiral.com>
2020-11-04 10:22:54 +1300

    code from morning lecture

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 import Greeter from './Greeter'
+
 
 class App extends React.Component {
 
   render() {
     return (
       <>
-        <h1>React development has begun!</h1>
-        <Greeter greeting="sup"/>
+        <Router>
+          <h1>React development has begun!</h1>
+          <Link to='/'>Home</Link>
+          <Link to='/greet/hi'>Hi</Link>
+          <Link to='/greet/yo'>Yo</Link>
+          <Route path='/' exact={true}>
+            <Greeter greeting="sup"/>
+          </Route>
+          <Route path='/greet/:greeting' exact={true} component={Greeter} />
+
+        </Router>
       </>
     )
   }
--- client/components/Greeter.jsx
+++ client/components/Greeter.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Greeter extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            greeting: props.greeting,
+            greeting: props.greeting || props.match.params.greeting,
             showGreeting: true
         }
     }
@@ -13,11 +14,19 @@ export default class Greeter extends React.Component {
         this.setState({showGreeting: !this.state.showGreeting})
     }
 
+    goSomewhere = () => {
+        this.props.history.push('/greet/LetMeOut')
+    }
+
     render() {
+        let greeting = this.props.greeting || this.props.match.params.greeting
+        console.log(this.props)
         let label = this.state.showGreeting ? 'hide' : 'show'
         return <div>
-            <h2>{this.state.showGreeting && this.state.greeting}</h2>
+            <h2>{this.state.showGreeting && greeting}</h2>
             <button onClick={this.toggleGreeting}>{label}</button>
+            <button onClick={this.goSomewhere}>Go Somewhere</button>
+            <Link to='/greet/yoza'>Yoza</Link>
         </div>
     }
 }
