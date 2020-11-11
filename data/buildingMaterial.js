c988a7270f86776ec6c98a958bb58e852f6339d0
Rowan-Potter <rowan@DESKTOP-04CLBHG.localdomain>
2020-11-12 12:45:06 +1300

    Added build mat and raid mat objects

---
+++ data/buildingMaterial.js
@@ -0,0 +1,54 @@
+const buildingMaterial = {
+    
+    wood: [
+
+        {
+            C4: 1,
+            Rockets: 2 ,
+            ExplosiveAmmo: 49,
+            SatchelCharge: 3
+
+
+        }
+
+    ],
+    stone: [
+        {
+            C4: 2,
+            Rockets: 4,
+            ExplosiveAmmo: 209,
+            SatchelCharge: 10
+        }
+
+    ],
+    metal: [
+        {
+            C4: 4,
+            Rockets: 8,
+            ExplosiveAmmo: 417,
+            SatchelCharge: 23
+
+        }
+    ],
+    armoredWall: [
+        {
+            C4: 8,
+            Rockets: 15,
+            ExplosiveAmmo: 834,
+            SatchelCharge: 46
+        }
+    ]
+ 
+
+
+    
+}
+    
+
+
+
+    
+
+
+export default buildingMaterial
+
---
+++ data/raidMaterial .js
@@ -0,0 +1,32 @@
+const raidMaterial = {
+    c4: [
+
+        {
+            MetalFrags: 200,
+            Sulfer: 2200,
+            Charcoal: 3000
+
+        }
+    ],
+    Rockets: [
+        {
+            MetalFrags: 100,
+            Sulfer: 1400,
+            Charcoal: 1950
+        }
+    ],
+    ExplosiveAmmo: [
+        {
+            MetalFrags: 10,
+            Sulfer: 50,
+            Charcoal: 60
+        }
+    ],
+    SatchelCharge: [
+        {
+            MetalFrags: 80,
+            Sulfer: 480,
+            Charcoal: 720
+        }
+    ]
+}
